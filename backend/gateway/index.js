159899c26263bab29aa833ba8b46b71263c64eb8
NikhilG-14 <goyal.nikhil0409@gmail.com>
2026-07-31 19:18:09 +0530

    Create complete chat service

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -6,6 +6,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import {getCurrentUser} from "./controllers/user.controller.js"
 import protect from "./middleware/auth.middleware.js"
+import { proxyWithHeader } from "./utils/proxyWithHeader.js"
 
 const port = process.env.PORT
 
@@ -17,6 +18,7 @@ app.use(cors({
 
 app.use(cookieParser())
 app.use("/api/auth", proxy(process.env.AUTH_SERVICE))
+app.use("/api/chat", protect, proxyWithHeader(process.env.CHAT_SERVICE))
 app.get("/api/me", protect, getCurrentUser)
 
 app.get("/", (req, res) => {
---
+++ backend/services/chat/controllers/chat.controller.js
@@ -0,0 +1,69 @@
+import Conversation from "../models/conversation.model.js"
+import Message from "../models/message.model.js"
+
+export const createConversation = async (req, res) => {
+    try {
+        const userId = req.headers["x-user-id"]
+        console.log("userId", userId)
+        const conversation = await Conversation.create({
+            userId: userId
+        })
+
+        return res.status(200).json(conversation)
+    } catch (error) {
+        return res.status(500).json({message: `Create conversation error ${error}`})
+    }
+}
+
+export const getConversations = async (req, res) => {
+    try {
+        const userId = req.headers["x-user-id"]
+        console.log("userId", userId)
+        const conversations = await Conversation.find({
+            userId: userId
+        }).sort({updatedAt: -1})
+
+        return res.status(200).json(conversation)
+    } catch (error) {
+        return res.status(500).json({message: `Get conversation error ${error}`})
+    }
+}
+
+export const updateConversation = async (req, res) => {
+    try {
+        const {id, title} = req.body
+        const conversations = await Conversation.findByIdAndUpdate(id, {
+            title
+        })
+
+        return res.status(200).json(conversation)
+    } catch (error) {
+        return res.status(500).json({message: `Update conversation error ${error}`})
+    }
+}
+
+export const saveMessage = async(req, res) => {
+    try {
+        const {conversationId, role, content} = req.body
+        const Message = await Message.create({
+            conversationId,
+            content,
+            role
+        })
+        return res.status(200).json(message)
+    } catch (error) {
+        return res.status(500).json({message: `Save message error ${error}`})
+    }
+}
+
+export const getMessages = async(req, res) => {
+    try {
+        const messages = await Message.find({
+            conversationId: req.params.conversationId
+        }).sort({createdAt: -1})
+        return res.status(200).json(messages)
+    } catch (error) {
+        return res.status(500).json({message: `get messages error ${error}`})
+    }
+}
+
