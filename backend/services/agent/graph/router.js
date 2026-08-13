ac8d618b7e6b5e0507176e71e0d7c9e11e10a821
NikhilG-14 <goyal.nikhil0409@gmail.com>
2026-08-13 17:15:09 +0530

    Most of the agent work is done

--- backend/services/agent/graph/router.js
+++ backend/services/agent/graph/router.js
@@ -1,7 +1,70 @@
 import { getModel } from "../config/llmModels.js"
 
 export const router = async (state) => {
-    const llm = getModel("router")
-    const prompt = 
+    const llm = await getModel("router")
+    const prompt = `You are an agent router.
+    
+    Available agents:
+
+    - chat
+    - search
+    - coding
+    - pdf
+    - ppt
+    - vision
+
+    Rules:
+
+    chat:
+    General conversation,
+    explainations,
+    learning,
+    questions.
+
+    search:
+    Current events,
+    latest information,
+    news,
+    recent developments,
+    internet lookup.
+
+    coding:
+    Generate code, 
+    debug code,
+    build projects, 
+    architecture,
+    API design.
+
+    pdf:
+    Questions about generate PDFs
+    or document context.
+
+    ppt:
+    Questrions about generate PPTs
+    or ppt context.
+
+    vision:
+    Generate image,
+    create image
+
+    Return ONLY one word:
+
+    chat
+    search
+    coding 
+    pdf
+    ppt
+    vision
+
+    User Query:
+    {${state.prompt}}
+    `
+
+    const response = await llm.invoke(prompt)
+    console.log(response)
+    return {
+        ...state,
+        agent: response.content.trim().toLowerCase()
+    }
 
 }
